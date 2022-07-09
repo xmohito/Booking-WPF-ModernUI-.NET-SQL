9a311589fe173b3f47bdf237b5a0542390aa1f51
x_mohito <brozda.it@gmail.com>
2022-07-09 15:32:44 +0200

    added send mail function via contact form

--- WpfApp1/AskForm.xaml.cs
+++ WpfApp1/AskForm.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Net.Mail;
 
 namespace WpfApp1
 {
@@ -32,6 +33,44 @@ namespace WpfApp1
         {
             this.MouseLeftButtonDown += delegate { DragMove(); };
 
+        }
+
+
+        private void sendMail()
+        {
+            string body = "<h3>Klient, " + email_txt.Text + " zadał zapytanie<br/><br/><br/></h3>";
+            body += "Treść: <br/>" + context_txt.Text + "<br/>";
+
+            try
+            {
+                using (MailMessage mail = new MailMessage())
+                {
+                    mail.From = new MailAddress("vip.apartaments@interia.pl", "Formularz Vip Apartaments");
+                    mail.To.Add("vip.apartaments@interia.pl");
+                    mail.Subject = topic_txt.Text;
+                    mail.Body = body;
+                    mail.IsBodyHtml = true;
+
+                    using (SmtpClient smtp = new SmtpClient("poczta.interia.pl", 587))
+                    {
+                        smtp.Credentials = new System.Net.NetworkCredential("vip.apartaments@interia.pl", "vipapartament12345");
+                        smtp.EnableSsl = true;
+                        smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                        MessageBox.Show("Twoje zapytanie zostało przesłane", "Wysłano", MessageBoxButton.OK, MessageBoxImage.Information);
+                        smtp.Send(mail);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void SendBtn_Click(object sender, RoutedEventArgs e)
+        {
+            sendMail();
         }
     }
 }
